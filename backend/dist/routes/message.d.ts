7206037aedd7352020e279ab7a632cc44f353e9b
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2025-10-10 15:10:20 +0530

    Remove dist folder from tracking

--- backend/dist/routes/message.d.ts
+++
@@ -1,3 +0,0 @@
-declare const messageRouter: import("express-serve-static-core").Router;
-export default messageRouter;
-//# sourceMappingURL=message.d.ts.map
--- backend/dist/routes/room.d.ts
+++
@@ -1,3 +0,0 @@
-declare const roomRouter: import("express-serve-static-core").Router;
-export default roomRouter;
-//# sourceMappingURL=room.d.ts.map
